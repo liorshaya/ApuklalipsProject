916f5ce1086c8feb218cdc8f4a8324d284c44c56
Nehorai0258 <nehorai0258@gmail.com>
2025-04-14 16:47:08 +0300

    regular

--- src/MainScene.java
+++ src/MainScene.java
@@ -1,7 +1,6 @@
 import javax.swing.*;
 import java.awt.*;
 import java.awt.image.BufferedImage;
-import java.util.PropertyResourceBundle;
 import java.util.Random;
 
 public class MainScene extends JPanel {
